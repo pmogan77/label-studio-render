de2364abff6022b6d085a76bcdc313c3b350ab69
Praveen Mogan <pmogan77@gmail.com>
2025-11-08 02:55:00 -0600

    init admin

--- start.sh
+++ start.sh
@@ -4,18 +4,17 @@ set -e
 # -------------------------------
 # AWS S3 storage configuration
 # -------------------------------
-export LABEL_STUDIO_STORAGE_BACKEND=s3
-export LABEL_STUDIO_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export LABEL_STUDIO_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-export LABEL_STUDIO_AWS_REGION=$AWS_REGION
-export LABEL_STUDIO_S3_BUCKET=$AWS_BUCKET
-export LABEL_STUDIO_S3_INPUT_PATH=$S3_INPUT_PATH
-export LABEL_STUDIO_S3_OUTPUT_PATH=$S3_OUTPUT_PATH
+export STORAGE_TYPE=s3
+export STORAGE_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export STORAGE_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export STORAGE_AWS_REGION_NAME=$AWS_REGION
+export STORAGE_AWS_BUCKET_NAME=$AWS_BUCKET
+export STORAGE_AWS_FOLDER=$S3_INPUT_PATH
 
 # -------------------------------
 # Enable invite-only registration
 # -------------------------------
-export LABEL_STUDIO_DISABLE_SIGNUP_WITHOUT_LINK=true
+export LABEL_STUDIO_DISABLE_SIGNUP_WITHOUT_LINK=false
 export CSRF_TRUSTED_ORIGINS="https://label-studio-render-cxdy.onrender.com,http://localhost:8080,http://127.0.0.1:8080,http://0.0.0.0:8080"
 export USE_ENFORCE_CSRF_CHECKS=false
 
